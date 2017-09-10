c8b8db53bf42cc2effefa638db06e3c786e29019
mauroparra <mauroparra12@gmail.com>
2017-09-09 20:51:31 -0500

    creando menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import { CustomersModule} from './customers/customers.module'
+import {OrdersModule} from './orders/orders.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { CurrentOptionComponent } from './current-option/current-option.component';
+import { AddComponent } from './orders/add/add.component';
+import {IndexComponent} from './customers/index/index.component';
+import { AddComponent as OrderAddComponent} from './orders/add/add.component'; 
 
+const appRoutes: Routes = [
+  {path:"customers/index", component : IndexComponent},
+  {path: "orders/add", component:OrderAddComponent},
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    CurrentOptionComponent
+    CurrentOptionComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
-    CustomersModule
+    CustomersModule,
+    OrdersModule,
+    RouterModule.forRoot( appRoutes )
   ],
   providers: [],
   bootstrap: [AppComponent]
